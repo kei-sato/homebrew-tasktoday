24b0406c82e90eac47058a62bd96883da27cd9f8
Kei Sato <kei-sato@meeetol.com>
2016-04-10 01:56:17 +0900

    update homebrew setting file

--- tasktoday.rb
+++ tasktoday.rb
@@ -1,8 +1,8 @@
 class Tasktoday < Formula
   desc "add tasks with its estimated time, then calculate end time. that's it."
-  homepage ""
-  url ""
-  sha256 ""
+  homepage "https://github.com/kei-sato/homebrew-tasktoday"
+  url "https://github.com/kei-sato/homebrew-tasktoday/raw/master/tasktoday"
+  sha256 "ec17b7f4465755adc1c96e837b5dca6cbd04b2f5e04ab2f436b3ef5192da4846"
   version "0.9.0"
 
   def install
