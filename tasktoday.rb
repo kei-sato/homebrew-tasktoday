10ec0b81306b0fdf80220fbc2f74edaf3a12956e
Kei Sato <kei-sato@meeetol.com>
2016-04-20 12:56:24 +0900

    add feature to remove the first 'not finished' task

--- tasktoday.rb
+++ tasktoday.rb
@@ -2,8 +2,8 @@ class Tasktoday < Formula
   desc "add tasks with its estimated time, then calculate end time. that's it."
   homepage "https://github.com/kei-sato/homebrew-tasktoday"
   url "https://github.com/kei-sato/homebrew-tasktoday/raw/master/tasktoday"
-  sha256 "ec17b7f4465755adc1c96e837b5dca6cbd04b2f5e04ab2f436b3ef5192da4846"
-  version "0.9.0"
+  sha256 "bd4608f21c513bd67e394434e4aae03794a39e2c56f81e8b049f1688dcc2d03b"
+  version "0.9.1"
 
   def install
     bin.install "tasktoday"
